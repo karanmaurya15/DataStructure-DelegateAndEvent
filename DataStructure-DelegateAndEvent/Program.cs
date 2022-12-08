adab13327d5f0ccbeb2ad41f72c242f6675d9890
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-08 16:12:49 +0530

    Wellcome to Program

---
+++ DataStructure-DelegateAndEvent/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructure_DelegateAndEvent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate and Events");
+        }
+    }
+}
